58693b531e1b3f10992640a838192b62f0767477
Peter Seggi <pedro.seggiaro@gmail.com>
2024-03-26 21:18:56 -0300

    childCount y varios demases para arrancar con la parte de select

--- master.c
+++ master.c
@@ -5,8 +5,20 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
+#define MAX_CHILDREN 20
 
-// este master genera en total 10 esclavos --> mejorable
+/*
+ Cuando creamos 2 pipes para conectar master a slave estas estan en el indice
+ childTag con este orden:
+    --------    1                         0  ----------
+   | Master |   ---------->---------------  |  Slave   |
+   |        |   ----------<---------------  |          |
+    --------    2                         3  ----------
+*/
+#define SLAVE_READ_END 0
+#define MASTER_WRITE_END 1
+#define MASTER_READ_END 2
+#define SLAVE_WRITE_END 3
 
 void pipeAndFork(int argc, char *argv[]);
 
@@ -23,75 +35,100 @@ int main(int argc, char *argv[]) {
 }
 
 void pipeAndFork(int argc, char *argv[]) {
-    // PipeFd[0] es el read end y [1] es el write end
-    int sendPipeFd[2];
-    if (pipe(sendPipeFd) == -1) {
-        perror("send pipe");
-        exit(EXIT_FAILURE);
-    }
+    int fileCount = argc - 1;
+    int processedCount = 0;
 
-    int receivePipeFd[2];
-    if (pipe(receivePipeFd) == -1) {
-        perror("receive pipe");
-        exit(EXIT_FAILURE);
-    }
+    // TO=DO IMPORTANTE ES HACER BIEN FILE COUNT, AHORA ESTA HARDCODEADO PARA
+    // HACERME LA VIDA FACIL 
+    
+    
+    //Puede ser un one liner pero esto es mas legible int
+    // childCount = fileCount * 0.1;  // -1 para sacar el ./master
 
-    pid_t cpid;
-    int status;
+    int childCount = 3;
+    childCount = (childCount > 20) ? 20 : childCount;
 
-    cpid = fork();
-    if (cpid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (cpid == 0) {
-        // Primero cierro los que no se usan en el slave
-        close(sendPipeFd[1]);
-        close(receivePipeFd[0]);
+    // ese *4 esta porq cada hijo va a tener 2 pipes -> 4fds
+    // El orden siempre va a ser masterRead, masterWrite, slaveRead, slaveWrite
+    int fileDescriptors[childCount * 4];
 
-        // Vamos a hacer un execve para llamar el slave
-        // Notar que un conjunto vacio totalmente rompe execv
-        char *arges[] = {NULL};
+    // creo childCount procesos y a cada uno inicialmente le voy pasando 2
+    // archivos
+    for (int i = 0; i < childCount; i++) {
+        int childTag = i * 4;
 
-        if (dup2(sendPipeFd[0], STDIN_FILENO) < 0 ||
-            dup2(receivePipeFd[1], STDOUT_FILENO) < 0) {
-            perror("dup");
+        if (pipe(&fileDescriptors[childTag]) == -1) {
+            perror("send pipe");
             exit(EXIT_FAILURE);
         }
 
-        // Execute the slave process
-        if (execv("./slave", arges) == -1) {
-            perror("execve");
+        if (pipe(&fileDescriptors[childTag + 2]) == -1) {
+            perror("receive pipe");
             exit(EXIT_FAILURE);
         }
-    } else {
-        // Parent process
-        // Wait for the child process to finish
-        char buffer[BUFFER_SIZE];
-        ssize_t bytesRead;
 
-        // Primero cierro los que no se usan en el master
-        close(sendPipeFd[0]);
-        close(receivePipeFd[1]);
+        pid_t cpid;
+        int status;
 
-        // esto de aca esta mal, es un fake select q espera a q el otro proceso
-        // termine porq sabe q ahi va a tener para leer
-        for (int i = 0; i < 3; i++){
-            write(sendPipeFd[1], files[i], strlen(files[i]));
-            sleep(2);
+        cpid = fork();
+        if (cpid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
 
-        close(sendPipeFd[1]);
+        else if (cpid == 0) {
+            // Primero cierro los que no se usan en el child que son mater read
+            // y master write
+            close(fileDescriptors[childTag + MASTER_READ_END]);
+            close(fileDescriptors[childTag + MASTER_WRITE_END]);
 
-        waitpid(cpid, &status, 0);
+            // Vamos a hacer un execve para llamar el slave
+            // Notar que un conjunto vacio por completo rompe execv
+            char *arges[] = {NULL};
 
-        // Check if the child process exited normally
-        if (WIFEXITED(status)) {
-            bytesRead = read(receivePipeFd[0], buffer, BUFFER_SIZE);
-            printf("%s\n", buffer);
-            printf("Child process exited with status %d\n",
-                   WEXITSTATUS(status));
+            if (dup2(fileDescriptors[childTag + SLAVE_READ_END], STDIN_FILENO) <
+                    0 ||
+                dup2(fileDescriptors[childTag + SLAVE_WRITE_END],
+                     STDOUT_FILENO) < 0) {
+                perror("dup");
+                exit(EXIT_FAILURE);
+            }
+
+            // Ejecutamos el slave process
+            if (execv("./slave", arges) == -1) {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
         } else {
-            printf("Child process exited abnormally\n");
+            // Parent process
+            // Wait for the child process to finish
+            char buffer[BUFFER_SIZE];
+            ssize_t bytesRead;
+
+            // Ahora cierro los que no se usan en el master que son slave read y
+            // slave write
+            close(fileDescriptors[childTag + SLAVE_READ_END]);
+            close(fileDescriptors[childTag + SLAVE_WRITE_END]);
+
+            // esto de aca esta mal, es un fake select q espera a q el otro
+            // proceso termine porq sabe q ahi va a tener para leer
+            write(fileDescriptors[childTag + MASTER_WRITE_END], files[i],
+                  strlen(files[i]));
+
+            close(fileDescriptors[childTag + MASTER_WRITE_END]);
+
+            waitpid(cpid, &status, 0);
+
+            // Nos fijamos que el child haya cortado bien
+            if (WIFEXITED(status)) {
+                bytesRead = read(fileDescriptors[childTag + MASTER_READ_END],
+                                 buffer, BUFFER_SIZE);
+                printf("%s\n", buffer);
+                printf("Child process exited with status %d\n",
+                       WEXITSTATUS(status));
+            } else {
+                printf("Child process exited abnormally\n");
+            }
         }
     }
     return;
